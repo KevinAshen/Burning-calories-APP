9c6c849220c846961cdc4cbf9c04e20355210a32
强淑婷 <863139668@qq.com>
2019-01-24 22:21:04 +0800

    日志界面部分完成qst

--- Burning-calories-APP/Burning-calories-APP/Classes/Log/Model/BCLLogHeaderView.m
+++ Burning-calories-APP/Burning-calories-APP/Classes/Log/Model/BCLLogHeaderView.m
@@ -7,60 +7,104 @@
 //
 
 #import "BCLLogHeaderView.h"
+#import <Masonry.h>
+static const CGFloat kLeftMargin = 39;
+static const CGFloat kTopMargin = 51;
 
 @interface BCLLogHeaderView ()
+
 @property (nonatomic, strong) UIButton *todayCaloriesButton;
-@property (nonatomic, strong) UILabel *anticipateCaloriesLabel;
-@property (nonatomic, strong) UILabel *healthConditionLabel;
 @property (nonatomic, strong) UIButton *anticipateCaloriesButton;
 @property (nonatomic, strong) UIButton *healthConditionButton;
+@property (nonatomic, strong) UILabel *anticipateCaloriesLabel;
+@property (nonatomic, strong) UILabel *healthConditionLabel;
 
 @end
 
 @implementation BCLLogHeaderView
 - (instancetype)initWithFrame:(CGRect)frame {
     if(self = [super initWithFrame:frame]) {
+        self.backgroundColor = [UIColor colorWithRed:1.00f green:0.29f blue:0.16f alpha:1.00f];
+        
+        self.anticipateCaloriesButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self addSubview:_anticipateCaloriesButton];
+        [_anticipateCaloriesButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self).offset(kLeftMargin);
+            make.top.equalTo(self).offset(kTopMargin);
+            make.width.mas_equalTo(63);
+            make.height.mas_equalTo(22);
+        }];
+        [_anticipateCaloriesButton setTitle:@"今日预期" forState:UIControlStateNormal];
+        _anticipateCaloriesButton.titleLabel.font = [UIFont systemFontOfSize:15];
+        
+        
         self.todayCaloriesButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [self addSubview:_todayCaloriesButton];
         [_todayCaloriesButton mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.equalTo(self).offset(140);
+            make.left.equalTo(self->_anticipateCaloriesButton.mas_right).offset(34);
             make.top.equalTo(self).offset(20);
             make.width.mas_equalTo(100);
             make.height.mas_equalTo(100);
         }];
+        _todayCaloriesButton.backgroundColor = [UIColor whiteColor];
         _todayCaloriesButton.layer.masksToBounds = YES;
         _todayCaloriesButton.layer.cornerRadius = 50;
-        [_todayCaloriesButton setTitle:@"今日卡路里" forState:UIControlStateNormal];
         
-        self.anticipateCaloriesLabel = [[UILabel alloc]init];
-        [self addSubview:_anticipateCaloriesLabel];
+        NSMutableAttributedString *titleAttributedString = [[NSMutableAttributedString alloc]initWithString:@"今日卡路里\n1777千夫"];
+        _todayCaloriesButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        [titleAttributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:17] range:NSMakeRange(0, 5)];
+        [titleAttributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:13] range:NSMakeRange(6, 6)];
+        [titleAttributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.00f green:0.00f blue:0.00f alpha:1.00f]  range:NSMakeRange(0, 5)];
+        [titleAttributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.80f green:0.80f blue:0.80f alpha:1.00f] range:NSMakeRange(6, 6)];
+        NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+        paragraphStyle.alignment = NSTextAlignmentCenter;
+        [titleAttributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(6, 6)];
         
-        self.healthConditionLabel = [[UILabel alloc]init];
-        [self addSubview:_healthConditionLabel];
+        [_todayCaloriesButton setAttributedTitle:titleAttributedString forState:UIControlStateNormal];
+        [_todayCaloriesButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        
+        
         
         self.healthConditionButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [self addSubview:_healthConditionButton];
+        [_healthConditionButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self.todayCaloriesButton.mas_right).offset(54);
+            make.top.equalTo(self.anticipateCaloriesButton);
+            make.width.equalTo(self.anticipateCaloriesButton.mas_width);
+            make.height.equalTo(self.anticipateCaloriesButton.mas_height);
+        }];
+        [_healthConditionButton setTitle:@"健康状况" forState:UIControlStateNormal];
+        _healthConditionButton.titleLabel.font = [UIFont systemFontOfSize:15];
         
-        self.anticipateCaloriesButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [self addSubview:_anticipateCaloriesButton];
+        self.anticipateCaloriesLabel = [[UILabel alloc]init];
+        [self addSubview:_anticipateCaloriesLabel];
+        [_anticipateCaloriesLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self.anticipateCaloriesButton.mas_left);
+            make.top.equalTo(self.anticipateCaloriesButton.mas_bottom).offset(5);
+            make.width.mas_equalTo(66);
+            make.height.mas_equalTo(16);
+        }];
+        _anticipateCaloriesLabel.text = @"1777千卡";
+        _anticipateCaloriesLabel.textColor = [UIColor colorWithRed:1.00f green:0.67f blue:0.59f alpha:1.00f];
+        _anticipateCaloriesLabel.font = [UIFont systemFontOfSize:12];
+        
+        self.healthConditionLabel = [[UILabel alloc]init];
+        [self addSubview:_healthConditionLabel];
+        [_healthConditionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(self.healthConditionButton.mas_left);
+            make.top.equalTo(self.healthConditionButton.mas_bottom).offset(5);
+            make.width.mas_equalTo(46);
+            make.height.mas_equalTo(16);
+        }];
+        _healthConditionLabel.text = @"良好";
+        _healthConditionLabel.textColor = [UIColor colorWithRed:1.00f green:0.67f blue:0.59f alpha:1.00f];
+        _healthConditionLabel.font = [UIFont systemFontOfSize:12];
         
     }
     return self;
 }
-- (void)layoutSubviews {
-    [super layoutSubviews];
-    
-//    [_todayCaloriesButton mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.equalTo(self).offset(140);
-//        make.top.equalTo(self).offset(20);
-//        make.width.mas_equalTo(100);
-//        make.height.mas_equalTo(100);
-//    }];
-//    _todayCaloriesButton.layer.masksToBounds = YES;
-//    _todayCaloriesButton.layer.cornerRadius = 50;
-//    [_todayCaloriesButton setTitle:@"今日卡路里" forState:UIControlStateNormal];
-    //_todayCaloriesButton
-}
+
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
--- Burning-calories-APP/Burning-calories-APP/Classes/Log/View/BCLLogView.m
+++ Burning-calories-APP/Burning-calories-APP/Classes/Log/View/BCLLogView.m
@@ -7,6 +7,8 @@
 //
 
 #import "BCLLogView.h"
+#import "BCLLogDayWeekTableViewCell.h"
+#import "BCLLogHeaderView.h"
 
 @interface BCLLogView ()
 
@@ -16,26 +18,50 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     if(self = [super initWithFrame:frame]) {
         self.backgroundColor = [UIColor colorWithRed:0.99f green:0.28f blue:0.14f alpha:1.00f];
-        _tableView.dataSource = self;
-        BCLLogHeaderView *logHeaderView = [[BCLLogHeaderView alloc]initWithFrame:CGRectMake(0, 0,kDeviceWidth, 250)];
-        _tableView.tableHeaderView = logHeaderView;
-        [self addSubview:_tableView];
+       
+        [self setTableView:_tableView];
+        
     }
     return self;
 }
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+- (void)setTableView:(UITableView *)tableView {
+    tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 64, kDeviceWidth, kDeviceHeight) style:UITableViewStylePlain];
+    tableView.dataSource = self;
+    [tableView registerClass:[BCLLogDayWeekTableViewCell class] forCellReuseIdentifier:@"dayWeekCell"];
+    
+    BCLLogHeaderView *logHeaderView = [[BCLLogHeaderView alloc]initWithFrame:CGRectMake(0, 0, kDeviceWidth, 200.0/667 * kDeviceHeight)];
+    tableView.tableHeaderView = logHeaderView;
+    
+    tableView.showsVerticalScrollIndicator = NO;
+    tableView.showsHorizontalScrollIndicator = NO;
+    tableView.bounces = NO;
+    tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    
+    [self addSubview:tableView];
+    
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section { 
     return 3;
 }
 
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath { 
-    UITableViewCell *cell = [_tableView dequeueReusableCellWithIdentifier:@"sd" forIndexPath:indexPath];
-    if(!cell){
-        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"sd"];
+    BCLLogDayWeekTableViewCell *dayWeekCell = [_tableView dequeueReusableCellWithIdentifier:@"dayWeekCell" forIndexPath:indexPath];
+    if(!dayWeekCell){
+        dayWeekCell = [[BCLLogDayWeekTableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"dayWeekCell"];
+        
+        NSArray *timeRecordArray = [NSArray arrayWithObjects:@"日记录",@"周记录",@"月记录", nil];
+        NSArray *caloriesRecordArray = [NSArray arrayWithObjects:@"1777千卡",@"1777千卡",@"1777千卡",nil];
+        NSArray *circleBackgroudColorArray = [NSArray arrayWithObjects:[UIColor colorWithRed:0.31f green:0.91f blue:0.00f alpha:1.00f],[UIColor colorWithRed:0.34f green:0.49f blue:0.90f alpha:1.00f],[UIColor colorWithRed:1.00f green:0.26f blue:0.00f alpha:1.00f], nil];
+        dayWeekCell.timeRecordLabel.text = timeRecordArray[indexPath.row];
+        dayWeekCell.caloriesRecordLabel.text = caloriesRecordArray[indexPath.row];
+        dayWeekCell.circleButton.backgroundColor = circleBackgroudColorArray[indexPath.row];
+        dayWeekCell.accessoryType=UITableViewCellAccessoryDisclosureIndicator;
+        
     }
-    return  cell;
+    return  dayWeekCell;
 }
 
 /*
