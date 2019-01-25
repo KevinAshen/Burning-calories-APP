dbf4e7635d8f91586c580d2dfc28fa8799326c8e
强淑婷 <863139668@qq.com>
2019-01-25 22:24:17 +0800

    日志cell高度qst

--- Burning-calories-APP/Burning-calories-APP/Classes/Log/View/BCLLogMonthTableViewCell.m
+++ Burning-calories-APP/Burning-calories-APP/Classes/Log/View/BCLLogMonthTableViewCell.m
@@ -1,16 +1,17 @@
 //
-//  BCLLogDayWeekTableViewCell.m
+//  BCLLogMonthTableViewCell.m
 //  Burning-calories-APP
 //
-//  Created by 强淑婷 on 2019/1/24.
+//  Created by 强淑婷 on 2019/1/25.
 //  Copyright © 2019年 J&Q. All rights reserved.
 //
 
-#import "BCLLogDayWeekTableViewCell.h"
+#import "BCLLogMonthTableViewCell.h"
 #import <Masonry.h>
 
 static const CGFloat kTopMargin = 22;
-@implementation BCLLogDayWeekTableViewCell
+
+@implementation BCLLogMonthTableViewCell
 
 - (void)setupUI {
     
@@ -24,6 +25,7 @@ static const CGFloat kTopMargin = 22;
     }];
     _timeRecordLabel.textColor = [UIColor colorWithRed:0.00f green:0.00f blue:0.00f alpha:1.00f];
     _timeRecordLabel.font = [UIFont systemFontOfSize:18];
+    _timeRecordLabel.text = @"月记录";
     
     
     self.circleButton = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -36,17 +38,19 @@ static const CGFloat kTopMargin = 22;
     }];
     _circleButton.layer.masksToBounds = YES;
     _circleButton.layer.cornerRadius = 9;
+    _circleButton.backgroundColor = [UIColor colorWithRed:1.00f green:0.26f blue:0.00f alpha:1.00f];
     
     self.caloriesRecordLabel = [[UILabel alloc]init];
     [self addSubview:_caloriesRecordLabel];
     [_caloriesRecordLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.timeRecordLabel.mas_right).offset(20);
-        make.top.equalTo(self.timeRecordLabel);
-        make.width.mas_equalTo(83);
+        make.left.equalTo(self.timeRecordLabel.mas_left);
+        make.top.equalTo(self.timeRecordLabel.mas_bottom).offset(10);
+        make.width.mas_equalTo(107);
         make.height.mas_equalTo(25);
     }];
     _caloriesRecordLabel.textColor = [UIColor colorWithRed:0.87f green:0.87f blue:0.87f alpha:1.00f];
     _caloriesRecordLabel.font = [UIFont systemFontOfSize:18];
+    _caloriesRecordLabel.text = @"577789千卡";
 }
 
 - (void)awakeFromNib {
