2e3dc5a2a065d86ff398e541d5ac526e22ff97ec
jiangkaiwen <894912881@qq.com>
2019-02-25 23:17:48 +0800

    bugJKW

--- Burning-calories-APP/Burning-calories-APP/Classes/Community/Views/BCLImageListView.m
+++ Burning-calories-APP/Burning-calories-APP/Classes/Community/Views/BCLImageListView.m
@@ -78,16 +78,15 @@
     }
 }
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect {
+ // Drawing code
+ }
+ */
 
 @end
 
-
-@implementation BCLOneImageVIew
+@implementation BCLOneImageView
 
 @end
