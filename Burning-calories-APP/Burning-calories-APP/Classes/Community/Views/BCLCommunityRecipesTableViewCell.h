4795e780e09f48dd8f6a9b4aec23e310b2d9b117
强淑婷 <863139668@qq.com>
2019-03-07 20:12:12 +0800

    小传qst

---
+++ Burning-calories-APP/Burning-calories-APP/Classes/Community/Views/BCLCommunityRecipesTableViewCell.h
@@ -0,0 +1,21 @@
+//
+//  BCLCommunityRecipesTableViewCell.h
+//  Burning-calories-APP
+//
+//  Created by 强淑婷 on 2019/3/6.
+//  Copyright © 2019年 J&Q. All rights reserved.
+//
+
+#import "BCLBaseTableViewCell.h"
+
+@interface BCLCommunityRecipesTableViewCell : BCLBaseTableViewCell
+
+@property (nonatomic, strong) UIImageView *foodImageView;
+@property (nonatomic, strong) UIButton *iconImageButton;
+@property (nonatomic, strong) UILabel *nickNameLabel;
+@property (nonatomic, strong) UILabel *elapsedTimeLabel;
+@property (nonatomic, strong) UILabel *phoneModelsLabel;
+@property (nonatomic, strong) UILabel *foodNameLabel;
+@property (nonatomic, strong) UILabel *caloriesLabel;
+
+@end
